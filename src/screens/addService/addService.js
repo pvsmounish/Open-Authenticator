46551ee1484f2f24ded0dce9b86a1ced5503a0d6
Mounish Sai <pvsmounish@gmail.com>
2019-10-12 19:34:55 +0530

    :sparkles: Create Add Service Screen

---
+++ src/screens/addService/addService.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native';
+
+class AddServiceScreen extends Component {
+
+    render() {
+        return (
+            <>
+                <Text>Add Service Screen</Text>
+            </>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
+
+export default AddServiceScreen;
---
+++ src/screens/addService/index.js
@@ -0,0 +1,5 @@
+import AddServiceScreen from './addService';
+
+export {
+    AddServiceScreen,
+}
