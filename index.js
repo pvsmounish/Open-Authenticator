c0ef4082e05cea1e390029d541479468f1b8b23e
Mounish Sai <pvsmounish@gmail.com>
2019-10-12 21:34:32 +0530

    :zap: Add React Native Paper provider

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-/**
- * @format
- */
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
-import {AppRegistry} from 'react-native';
 import App from './src/app';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => {
+    return (
+        <PaperProvider>
+            <App />
+        </PaperProvider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
