b82fc068233840ba1cc4eb0b616fe6a6c4cc917c
Mounish Sai <pvsmounish@gmail.com>
2019-10-12 19:23:18 +0530

    :art: Add colors

---
+++ src/styles/colors.js
@@ -0,0 +1,34 @@
+export const grey = {
+    black: '#000000',
+    white: '#ffffff',
+    grey100: '#f5f5f5',
+    grey200: '#eeeeee',
+    grey300: '#e0e0e0',
+    grey400: '#bdbdbd',
+    grey500: '#9e9e9e',
+    grey600: '#757575',
+    grey700: '#616161',
+    grey800: '#424242',
+    grey900: '#212121',
+};
+
+export const social = {
+    facebook: '#3b5998',
+    gmail: '#DB4437',
+    instagram: '#CA2F86',
+    twitter: '#55ACEE',
+};
+
+export const notification = {
+    info: '#1261A0',
+    success: '#5CB85C',
+    warning: '#FFC75D',
+    danger: '#D8534F',
+};
+
+export const theme = {
+    lightViolet: '#7366FF',
+    lightPink: '#FF6CAB',
+    greyBlack: '#313131',
+    lightGrey: '#CECECE',
+}
