48df7ba67f87e3bcdc942e0fd8a1b498c6669a2e
Mounish Sai <pvsmounish@gmail.com>
2019-10-13 18:38:26 +0530

    :sparkles: Add getQueryParams URL Util

---
+++ src/utils/url/url.js
@@ -0,0 +1,9 @@
+export const getQueryParams = ({ url }) => {
+    const regex = /[?&]([^=#]+)=([^&#]*)/g;
+    let params = {};
+    let match;
+    while (match = regex.exec(url)) {
+        params[match[1]] = match[2];
+    }
+    return params;
+}
